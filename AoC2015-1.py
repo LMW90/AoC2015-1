eba29d0bd40c61ce7472a8a14b72d198c4c85fcd
Lukasz Wargocki <lmwargocki@gmail.com>
2022-05-07 10:56:00 +0200

    added comments

--- AoC2015-1.py
+++ AoC2015-1.py
@@ -1,13 +1,18 @@
+# assign file from same folder to variable
 filename = "inputDay-1.txt"
+# try creating file handler
 try:
     fhand = open(filename)
 except:
     print(f'Cannot open file: {filename}')
     exit()
+# create variable to load text file content
 text = ''
 count = 0
+# save read lines to a variable
 for line in fhand:
     text = text + line
+    # read chars from a variable - works within outer loop only cause there is only one line in a text file
     for char in text:
         if char == '(':
             count += 1
